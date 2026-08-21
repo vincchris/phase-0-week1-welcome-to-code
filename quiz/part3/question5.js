1543e3d9a7aa27f336d1df755163493426b5990a
Vincent Chriss <vchristianthen@gmail.com>
2026-08-21 21:58:08 +0700

    Selesai Arcane Quest Part 3

---
+++ quiz/part3/question5.js
@@ -0,0 +1,20 @@
+// Problem buatlah bintang seperti berikut
+// let input = 5
+//hasilnya
+//*
+//**
+//***
+//****
+//*****
+
+let input = 5
+
+for (let i = 1; i <= input; i++) {
+  let star = "";
+
+  for (let j = 1; j <= i; j++) {
+    star += "*"
+  }
+
+  console.log(star)
+}
